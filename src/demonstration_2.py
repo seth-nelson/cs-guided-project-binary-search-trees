8445e26223396b8f8859050f170ecefd975fbe14
Seth Nelson <seth.nelson@outlook.com>
2021-01-14 15:15:28 -0800

    finished other demo from class project

--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -1,43 +1,41 @@
 """
-You are given a binary tree. You need to write a function that can determin if
-it is a valid binary search tree.
-
-The rules for a valid binary search tree are:
-
-- The node's left subtree only contains nodes with values less than the node's
-value.
-- The node's right subtree only contains nodes with values greater than the
-node's value.
-- Both the left and right subtrees must also be valid binary search trees.
-
-Example 1:
-Input:
-
+You are given a binary tree.
+​
+Write a function that can find the **maximum depth** of the binary tree. The
+maximum depth can be defined as the number of nodes found along the longest
+path from the root down to the furthest leaf node. Remember, a leaf node is a
+node that has no children.
+​
+Example:
+​
+Given the following binary tree
+​
     5
    / \
-  3   7
-
-Output: True
-
-Example 2:
-Input:
-
-    10
-   / \
-  2   8
-     / \
-    6  12
-
-Output: False
-Explanation: The root node's value is 10 but its right child's value is 8.
+  12  32
+     /  \
+    8    4
+​
+your function should return the depth = 3.
 """
-# Definition for a binary tree node.
-class TreeNode:
+class BinaryTreeNode:
     def __init__(self, value=0, left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
-
-def is_valid_BST(self, root):
-    # Your code here
-
+​
+    def maxDepth(self):
+        # Your code here
+        # if no left or right
+        left_height = 0
+        right_height = 0
+​
+        if self.right:
+            right_height = self.right.maxDepth()
+​
+        if self.left:
+            left_height = self.left.maxDepth()
+​
+        largest_height = max(right_height, left_height)
+​
+        return 1 + largest_height
